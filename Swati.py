2247e827c1e027433f9e95582a386153c888a5c5
swatidhakaaa <swatidhaka5213@gmail.com>
2026-07-21 18:14:28 +0530

    Add files via upload

---
+++ Swati.py
@@ -0,0 +1,115 @@
+print("Hello World")
+print("Swati Dhaka")
+print("Swati is my name")
+print("My age is 19")
+print("Swati is my name","My age is 19")
+print(19)
+print(19+20)
+name="Swati"
+age=19
+price=25.99
+print(name)
+print(age)
+print("name")
+print("my name is :",name)
+print("my age is :",age)
+age2=age
+print(age2)
+print(type(name))
+print(type(age))
+print(type(price))
+name1=("sd")
+name2=('sd')
+name3=('''sd''')
+print(name1)
+print(name2)
+print(name3)
+age=19
+old=False
+a=None
+print(type(old))
+print(type(a))
+a=4
+b=3
+s=a+b
+print(s)
+#arithmetic operators
+a=6
+b=2
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a%b) #reminder
+print(a**b) #a^b
+#relational opertors
+a=23
+b=43
+print(a==b)#False
+print(a!=b)#True
+print(a>=b)#False
+print(a>b)#False
+print(a<b)#True
+print(a<=b)#True
+num=30
+num**=30
+print(num)
+print(not True)
+print(not False)
+val1=True
+val2=False
+print("and opertor:",val1 and val2)
+print("or opertor:",val1 or val2)
+val1=True
+val2=True
+print("and opertor:",val1 and val2)
+print("or opertor:",(a==b) or (a<b))
+d=int("2")
+e=3.45
+print(type(d))
+print(d+e)
+name=input("enter your name: ")
+print("Welcome",name)
+
+input("enter your name: ")
+int("5")
+val=float(input("enter some value:"))
+print(type(val),val)
+
+name=input("enter your name:")
+age=input("enter your age:")
+marks=input("enter your marks:")
+
+print("welcome",name)
+print("welcome",age)
+print("welcome",marks)
+
+a=int(input("enter first:"))
+b=int(input("enter second:"))
+print(a>=b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
