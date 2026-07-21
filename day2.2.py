6740efa6f48364ec4a0ea10fcbb83db9dc855616
Swati Dhaka <swatidhaka5213@gmail.com>
2026-07-21 18:41:43 +0530

    Add files via upload

---
+++ day2.2.py
@@ -0,0 +1,94 @@
+#light = "pink"
+
+#if(light == "red"):
+    #print("Stop")
+#elif(light == "green"):
+    #print("Go")
+#elif(light == "yellow"):
+    #print("Look")
+#else:
+    #print("Light is broken")
+    
+#age=14
+
+#if(age >= 18):
+ #   print("can vote")
+#else:
+ #   print("CANNOT vote")
+
+#marks=int(input("enter student marks:"))
+
+#if(marks >=90):
+ #   grade="A"
+#elif(marks >=80 and marks <90):
+ #   grade="B"
+#elif(marks >=70 and marks <80):
+ #   grade="C"
+#else:
+ #   grade="D"
+#print("grade of the student ->",grade)    
+
+
+#age=34
+
+#if(age >=18):
+ #   if(age >=80):
+  #      print("cannot drive")
+   # else:
+    #    print("can drive")
+#else:
+ #   print("cannot drive")
+
+
+#age=95
+
+#if(age >=18):
+ #   if(age >=80):
+  #      print("cannot drive")
+   # else:
+    #    print("can drive")
+#else:
+ #   print("cannot drive")
+
+
+#num=int(input("enter number:"))
+
+#rem=num % 2
+
+#if(rem == 0):
+ #   print("EVEN")
+#else:
+ #   print("ODD")
+
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+c=int(input("enter third number:"))
+d=int(input("enter fourth number:"))
+
+if(a>=b and a>=c):
+    print("first number is largest",a)
+elif(b>=c and b>=d):
+    print("second number is largest",b)
+elif(c>=d):
+    print("third is largest",c)
+else:
+    print("fourth is largest",d)
+
+#x=int(input("enter number:"))
+
+#if(x % 7 == 0):
+#    print("multiple of 7")
+#else:
+ #   print("not a multiple")
+
+
+
+
+
+
+
+
+
+
+
+
